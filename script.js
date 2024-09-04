22741c4455a523f878fa46d7f23b39c721c8eac3
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-04 23:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ function setup() {
 
 function draw() {
     background(220);
+    xoff = 0;
     for (let i = 0; i < cols; i++) {
         arrows[i] = [];
+        yoff = 0;
         for(let j = 0; j < rows; j++) {
-
+            let angle = noise(xoff, yoff);
+            text(round(angle, 2), size/2+i*size, size/2+j*size);
         }
     }
 }
