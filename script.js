0ba145975e671fdff58b4f44c8a5617225c81923
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-04 23:38:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let cols;
-let rows;
-let size =50;
+let cols;let rows;let size =50;
+let arrows = [];
+let xoff = 0; let yoff = 0;let increment = 0.1;
 
 function setup() {
     createCanvas(400, 400);
@@ -17,6 +17,8 @@ function draw() {
         for(let j = 0; j < rows; j++) {
             let angle = noise(xoff, yoff);
             text(round(angle, 2), size/2+i*size, size/2+j*size);
+            yoff += increment;
         }
+        xoff += increment
     }
 }
